b41f9741d8c433e7da93b8c62aa612c1d4185bca
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-11-15 01:18:17 +0300

    add header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,25 +1,38 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styles from './Header.module.css'
 
 export default function Header() {
   const { pathname } = useRouter()
 
   return (
-    <header>
-      <nav>
-        <svg viewBox="0 0 2 3" aria-hidden="true">
-          <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <svg className={styles.svg} viewBox="0 0 2 3" aria-hidden="true">
+          <path className={styles.path} d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
         </svg>
-        <ul>
-          <li className={pathname === '/' ? 'active' : ''}>
-            <a href={'/'}>Home</a>
+        <ul className={styles.ul}>
+          <li
+            className={
+              (pathname === '/' ? styles.active + ' ' : '') + styles.li
+            }
+          >
+            <a className={styles.a} href={'/'}>
+              Home
+            </a>
           </li>
-          <li className={pathname === '/about' ? 'active' : ''}>
-            <a href={'/about'}>About</a>
+          <li
+            className={
+              (pathname === '/about' ? styles.active + ' ' : '') + styles.li
+            }
+          >
+            <a className={styles.a} href={'/about'}>
+              About
+            </a>
           </li>
         </ul>
-        <svg viewBox="0 0 2 3" aria-hidden="true">
-          <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+        <svg className={styles.svg} viewBox="0 0 2 3" aria-hidden="true">
+          <path className={styles.path} d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
         </svg>
       </nav>
     </header>
