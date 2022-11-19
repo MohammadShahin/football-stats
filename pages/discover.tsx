f40a1692e7ed343288e53c3e686c2f3d3ed50e49
Karam19 <karam.shbeb1@hotmail.com>
2022-11-19 11:44:19 +0300

    add discover page

---
+++ pages/discover.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Discover: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Discover your favorite league</h1>
+    </div>
+  )
+}
+
+export default Discover
