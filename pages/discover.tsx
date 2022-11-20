d2299c5433edf164f96a7c07d4a43cebf4f70ee9
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-11-20 23:55:39 +0300

    removed unneed styles

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next';
 import LeaguePicker from '../src/components/LeaguePicker';
-import styles from '../styles/Home.module.css';
 
 const Discover: NextPage = () => (
-  <div className={styles.container}>
+  <div>
     <h1>Discover your favorite league</h1>
     <LeaguePicker />
   </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
 import LastSeasonWinners from '../src/components/LastSeasonWinners';
 import RandomFact from '../src/components/RandomFact';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
-  <div className={styles.container}>
+  <div>
     <h1>Welcome to Football Stats app!</h1>
     <RandomFact />
     <br />
